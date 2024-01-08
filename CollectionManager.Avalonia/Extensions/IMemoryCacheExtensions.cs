efe0e380e6980a9e1344c43f0331c852e61f0b01
Wayla <nikosnaoum127@gmail.com>
2024-01-08 02:20:27 +0200

    made the extensions work.

--- CollectionManager.Avalonia/Extensions/IMemoryCacheExtensions.cs
+++ CollectionManager.Avalonia/Extensions/IMemoryCacheExtensions.cs
@@ -8,34 +8,36 @@ namespace CollectionManager.Avalonia.Extensions;
 
 internal static class IMemoryCacheExtensions
 {
-    internal static ReadOnlyCollection<OsdbCollection> GetAllCollections(IMemoryCache cache)
+    internal static ReadOnlyCollection<OsdbCollection> GetAllCollections(this IMemoryCache cache)
         => cache.Get<ReadOnlyCollection<OsdbCollection>>(IMemoryCacheKeys.Collections)!;
 
-    internal static ReadOnlyCollection<OsuBeatmap> GetAllBeatmaps(IMemoryCache cache)
+    internal static ReadOnlyCollection<OsuBeatmap> GetAllBeatmaps(this IMemoryCache cache)
         => cache.Get<ReadOnlyCollection<OsuBeatmap>>(IMemoryCacheKeys.Beatmaps)!;
 
-    internal static ReadOnlyCollection<OsdbCollection> QueryCollections(IMemoryCache cache, string query)
+    internal static ReadOnlyCollection<OsdbCollection> QueryCollections(this IMemoryCache cache, string query)
         => GetAllCollections(cache).Where(x => x.Name.Contains(query)).ToList().AsReadOnly();
 
-    internal static ReadOnlyCollection<OsuBeatmap> QueryBeatmaps(IMemoryCache cache, string query)
+    internal static ReadOnlyCollection<OsuBeatmap> QueryBeatmaps(this IMemoryCache cache, string query)
         => GetAllBeatmaps(cache).Where(x => x.Title.Contains(query)).ToList().AsReadOnly();
 
-    internal static void AddCollections(IMemoryCache cache, ReadOnlyCollection<OsdbCollection> newCollections)
+    internal static void AddCollections(this IMemoryCache cache, ReadOnlyCollection<OsdbCollection> newCollections)
         => cache.Set(IMemoryCacheKeys.Collections, newCollections);
 
-    internal static void AddBeatmaps(IMemoryCache cache, ReadOnlyCollection<OsuBeatmap> newBeatmaps)
+    internal static void AddBeatmaps(this IMemoryCache cache, ReadOnlyCollection<OsuBeatmap> newBeatmaps)
         => cache.Set(IMemoryCacheKeys.Beatmaps, newBeatmaps);
 
-    internal static bool IsSame<T, TItem>(IMemoryCache cache, string key, T comparableEnumerable) where T : IEnumerable<TItem>
+    internal static bool IsSame<T, TItem>(this IMemoryCache cache, string key, T comparableEnumerable) where T : IEnumerable<TItem>
         => cache.TryGetValue(key, out T? cachedEnumerable) && cachedEnumerable is not null && cachedEnumerable.SequenceEqual(comparableEnumerable);
 
-    internal static bool IsSame<T>(IMemoryCache cache, string key, T comparableValue)
+    internal static bool IsSame<T>(this IMemoryCache cache, string key, T comparableValue)
         => cache.TryGetValue(key, out T? cachedValue) && cachedValue is not null && cachedValue.Equals(comparableValue);
+}
 
-    internal sealed class IMemoryCacheKeys
-    {
-        internal static string Beatmaps => "all-beatmaps";
+internal sealed class IMemoryCacheKeys
+{
+    internal static string DatabaseFilePath => "database-filepath";
 
-        internal static string Collections => "all-collections";
-    }
+    internal static string Beatmaps => "all-beatmaps";
+
+    internal static string Collections => "all-collections";
 }
