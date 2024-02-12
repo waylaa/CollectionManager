119765bda3851ccf98e4e56fb55f59620785d177
wayla <nikosnaoum127@gmail.com>
2024-02-13 00:22:53 +0200

    main view impl

--- CollectionManager.Avalonia/Views/MainView.axaml.cs
+++ CollectionManager.Avalonia/Views/MainView.axaml.cs
@@ -1,22 +1,13 @@
-﻿using Avalonia.ReactiveUI;
+﻿using Avalonia.Controls;
 using CollectionManager.Avalonia.ViewModels;
-using ReactiveUI;
 
 namespace CollectionManager.Avalonia.Views;
 
-public partial class MainView : ReactiveUserControl<MainViewViewModel>
+public partial class MainView : UserControl
 {
     public MainView()
     {
         InitializeComponent();
-
-        this.WhenActivated(registration =>
-        {
-            this.BindCommand(ViewModel, x => x.LoadCollectionsCommand, x => x.AddCollections);
-            this.BindCommand(ViewModel, x => x.LoadDatabaseCommand, x => x.AddDatabase);
-
-            this.OneWayBind(ViewModel, x => x.LoadedCollections, x => x.Collections.ItemsSource);
-            this.OneWayBind(ViewModel, x => x.LoadedBeatmaps, x => x.Beatmaps.ItemsSource);
-        });
+        DataContext = new MainViewViewModel();
     }
 }
