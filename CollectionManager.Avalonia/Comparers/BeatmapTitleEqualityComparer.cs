c18af36e1c5d901bfe822498ef080288f1e980c4
wayla <nikosnaoum127@gmail.com>
2024-02-13 00:19:50 +0200

    Added equality comparers for models so they can be used by the IsSame methods

---
+++ CollectionManager.Avalonia/Comparers/BeatmapTitleEqualityComparer.cs
@@ -0,0 +1,14 @@
+﻿using CollectionManager.Core.Models;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace CollectionManager.Avalonia.Comparers;
+
+internal sealed class BeatmapTitleEqualityComparer : IEqualityComparer<OsuBeatmap>
+{
+    public bool Equals(OsuBeatmap? x, OsuBeatmap? y)
+        => x is not null && y is not null && x.Title == y.Title;
+
+    public int GetHashCode([DisallowNull] OsuBeatmap obj)
+        => obj.GetHashCode();
+}
---
+++ CollectionManager.Avalonia/Comparers/CollectionNameEqualityComparer.cs
@@ -0,0 +1,14 @@
+﻿using CollectionManager.Core.Models;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace CollectionManager.Avalonia.Comparers;
+
+internal sealed class CollectionNameEqualityComparer : IEqualityComparer<OsdbCollection>
+{
+    public bool Equals(OsdbCollection? x, OsdbCollection? y)
+        => x is not null && y is not null && x.Name == y.Name;
+
+    public int GetHashCode([DisallowNull] OsdbCollection obj)
+        => obj.GetHashCode();
+}
